607e2fbf28d40e5f90261051d2a9c42a79fd5f48
Manju Priyadharsini <manjupriyadharsini@ali-6c96cfe097b1.local>
2018-04-24 13:26:07 +0800

    Project files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey There, I’m going to run in the pipeline using docker-compose !" );
+    }
+}
